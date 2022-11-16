10e808b7f02b9ecd4ae0dc975ed03895bb591e04
life-paradox <zhideevpavel@gmail.com>
2022-11-16 22:20:58 +0200

    remake on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,71 +1,66 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 import css from './App.module.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-export default class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [positivePercentage, setPositivePercentage] = useState(0);
 
-    this.state = {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    };
-  }
+  const incrementValue = evt => {
+    const currentButton = evt.target.textContent;
 
-  incrementValue = evt => {
-    const curId = evt.target.textContent.toLowerCase();
-
-    this.setState(prevState => {
-      return {
-        [curId]: prevState[curId] + 1,
-      };
-    }, this.countTotalFeedback);
+    switch (currentButton) {
+      case 'Good':
+        setGood(prevState => prevState + 1);
+        setTotal(prevState => prevState + 1);
+        break;
+      case 'Neutral':
+        setNeutral(prevState => prevState + 1);
+        setTotal(prevState => prevState + 1);
+        break;
+      default:
+        setBad(prevState => prevState + 1);
+        setTotal(prevState => prevState + 1);
+    }
   };
 
-  countTotalFeedback = () => {
-    this.setState(prevState => {
-      return { total: prevState.good + prevState.neutral + prevState.bad };
-    }, this.countPositiveFeedbackPercentage);
+  const countPositiveFeedbackPercentage = () => {
+    setPositivePercentage(Math.round((good / total) * 100) + '%');
   };
 
-  countPositiveFeedbackPercentage = () => {
-    this.setState(prevState => {
-      return {
-        positivePercentage:
-          Math.round((prevState.good / prevState.total) * 100) + '%',
-      };
-    });
-  };
+  useEffect(countPositiveFeedbackPercentage, [good, neutral, bad]);
 
-  render() {
-    return (
-      <div className={css.feedbackContainer}>
-        <Section title="Please leave feedback" level={1}>
-          <FeedbackOptions
-            options={['Good', 'Neutral', 'Bad']}
-            onLeaveFeedback={this.incrementValue}
+  return (
+    <div className={css.feedbackContainer}>
+      <Section title="Please leave feedback" level={1}>
+        <FeedbackOptions
+          options={['Good', 'Neutral', 'Bad']}
+          onLeaveFeedback={incrementValue}
+        />
+      </Section>
+      <Section title="Statistics" level={2}>
+        {total === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
           />
-        </Section>
-        <Section title="Statistics" level={2}>
-          {!this.state.total ? (
-            <Notification message="There is no feedback" />
-          ) : (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.state.total}
-              positivePercentage={this.state.positivePercentage}
-            />
-          )}
-        </Section>
-      </div>
-    );
-  }
+        )}
+      </Section>
+    </div>
+  );
+};
 
-  state = {};
-}
+export default App;
